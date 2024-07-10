64d3e9a9d8d078bbdc23cee0eab5ad26a073ec78
Beatriz Salles <beatrizsallesss@gmail.com>
2024-07-10 20:27:33 -0300

    feat: 🎸 Add gifts list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import Card from "./components/card";
 import "./styles/App.scss";
+import giftList from "./gift-list.json";
 
 function App() {
+  const { gifts } = giftList;
+
+  console.log("=> ", gifts);
   return (
     <section className="main-container">
       <header>
         <h1>Presentes de Casa Nova</h1>
       </header>
       <article>
-        <Card />
+        {gifts.map((gift) => (
+          <Card name={gift.name} image={gift.image} link={gift.link} />
+        ))}
       </article>
     </section>
   );
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,20 +1,21 @@
 import "./styles.scss";
 
-export default function Card() {
+interface ICardProps {
+  image: { src: string; alt: string };
+  name: string;
+  link?: string;
+}
+
+export default function Card({ image, name, link }: ICardProps) {
   return (
     <div className="card-container">
-      <img
-        src="https://m.media-amazon.com/images/I/51sTCIytT9L._AC_SL1200_.jpg"
-        alt="Imagem de uma pipoqueira vermelha da Tramontina"
-      />
-      <p>Pipoqueira</p>
-      <a
-        href="https://www.amazon.com.br/Tramontina-PIPOQUEIRA-20CM-LORETO-VERMELHO/dp/B09W4CSX53/ref=asc_df_B09W4CSX53/?tag=googleshopp00-20&linkCode=df0&hvadid=555495720867&hvpos=&hvnetw=g&hvrand=13161590242518066902&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9197317&hvtargid=pla-1648147887490&psc=1&mcid=a8e89279af9331418bd701d92cbb4116"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Sugestão de compra
-      </a>
+      <img src={image.src} alt={image.alt} />
+      <p>{name}</p>
+      {link && (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          Sugestão de compra
+        </a>
+      )}
       <button>Presentear</button>
     </div>
   );
